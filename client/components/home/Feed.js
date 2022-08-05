09a4a3fd8f9e31ce80068d58789df29d17fe4729
Andrei Ujica <andrei@vipra.tech>
2022-08-05 14:37:58 +0300

    feat(client): add TweetBox component

--- client/components/home/Feed.js
+++ client/components/home/Feed.js
@@ -1,4 +1,5 @@
 import { BsStars } from "react-icons/bs";
+import TweetBox from "./TweetBox";
 
 const style = {
   wrapper: `flex-[2] border-r border-l border-[#38444d] overflow-y-scroll`,
@@ -13,6 +14,7 @@ function Feed() {
         <div className={style.headerTitle}>Home</div>
         <BsStars />
       </div>
+      <TweetBox />
     </div>
   );
 }
