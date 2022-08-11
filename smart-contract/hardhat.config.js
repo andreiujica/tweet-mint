4643f0873c0eaa89d8dfe801be291297e2a1537e
Andrei Ujica <andrei@vipra.tech>
2022-08-11 21:11:59 +0300

    feat(smart-contract):add config settings(alchemy url and metamask)

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.2",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/HA2tEuNsadGfwBCRgyFrT6tpxgzMgG6z",
+      accounts: [
+        "51a7bfbb0c6afd2faa61a81d6115e9639c0f301bacdf36d8decffe632187afc6",
+      ],
+    },
+  },
 };
