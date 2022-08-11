019f566294812d92d8f57e25c3da371154b4515f
Andrei Ujica <andrei@vipra.tech>
2022-08-11 21:56:45 +0300

    feat(client): add state and methods for metamask auth

---
+++ client/context/TwitterContext.js
@@ -0,0 +1,68 @@
+import { createContext, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+export const TwitterContext = createContext();
+
+export const TwitterProvider = ({ children }) => {
+  const [appStatus, setAppStatus] = useState("loading");
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  /**
+   * Checks if there is an active wallet connection
+   */
+  const checkIfWalletIsConnected = async () => {
+    if (!window.ethereum) return setAppStatus("noMetaMask");
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (addressArray.length > 0) {
+        setAppStatus("connected");
+        setCurrentAccount(addressArray[0]);
+      } else {
+        router.push("/");
+        setAppStatus("notConnected");
+      }
+    } catch (err) {
+      setAppStatus("error");
+    }
+  };
+
+  /**
+   * Initiates MetaMask wallet connection
+   */
+  const connectWallet = async () => {
+    if (!window.ethereum) return setAppStatus("noMetaMask");
+    try {
+      setAppStatus("loading");
+
+      const addressArray = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      if (addressArray.length > 0) {
+        setAppStatus("connected");
+        setCurrentAccount(addressArray[0]);
+      } else {
+        router.push("/");
+        setAppStatus("notConnected");
+      }
+    } catch (err) {
+      setAppStatus("error");
+    }
+  };
+
+  return (
+    <TwitterContext.Provider
+      value={{ appStatus, currentAccount, connectWallet }}
+    >
+      {children}
+    </TwitterContext.Provider>
+  );
+};
