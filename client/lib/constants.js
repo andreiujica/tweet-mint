fbcfca1b131e572b5bb33df92d0b3c7862485f2f
Andrei Ujica <andrei@vipra.tech>
2022-08-11 21:54:22 +0300

    feat(client): import nft minter address

---
+++ client/lib/constants.js
@@ -0,0 +1,4 @@
+import contractArtifact from "./ProfileImageNfts.json";
+
+export const contractAddress = "0x35C6F410D61Ba56245393473d9Cc686b7e630307";
+export const contractABI = contractArtifact.abi;
